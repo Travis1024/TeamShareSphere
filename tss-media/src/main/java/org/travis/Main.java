d9bbed8f39ec3f707aa778c0fb7505c0d0b97a65
travis <travis_x@163.com>
2024-04-19 16:56:20 +0800

    init-project

---
+++ tss-media/src/main/java/org/travis/Main.java
@@ -0,0 +1,14 @@
+package org.travis;
+
+/**
+ * @ClassName ${NAME}
+ * @Description TODO
+ * @Author travis-wei
+ * @Version v1.0
+ * @Data 2024/4/19
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

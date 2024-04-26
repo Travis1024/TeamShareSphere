36eb94f127da367c685af1154430d55e9e661231
travis <travis_x@163.com>
2024-04-26 17:37:32 +0800

    init-project

--- tss-auth/tss-auth-service/src/main/java/org/travis/auth/handler/ServiceDegradedHandler.java
+++ tss-auth/tss-auth-service/src/main/java/org/travis/auth/handler/ServiceDegradedHandler.java
@@ -14,6 +14,9 @@ import org.travis.common.exceptions.ServiceDegradedException;
  */
 @Slf4j
 public class ServiceDegradedHandler {
+    /**
+     * 非同类需要配置为静态方法，否则找不到对应方法；
+     */
     public static String commonBlockHandler(BlockException blockException) {
         log.warn("[服务降级-commonBlockHandler] -> {}", blockException.toString());
         throw new ServiceDegradedException("「服务降级」请稍后重试");

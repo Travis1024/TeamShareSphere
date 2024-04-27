4b4c7b94f2be058088f8e8d38c304bb1776daa66
travis <travis_x@163.com>
2024-04-27 12:17:55 +0800

    init-project

--- tss-auth/tss-auth-service/src/main/java/org/travis/auth/controller/LoginController.java
+++ tss-auth/tss-auth-service/src/main/java/org/travis/auth/controller/LoginController.java
@@ -72,7 +72,7 @@ public class LoginController {
         StpUtil.logout();
     }
 
-    @SentinelResource(value = "test1", blockHandlerClass = ServiceDegradedHandler.class, blockHandler = "commonBlockHandler")
+    @SentinelResource(value = "func-test1", blockHandlerClass = ServiceDegradedHandler.class, blockHandler = "commonBlockHandler")
     @GetMapping("/test1")
     public String testOne() {
         log.info("test1 -> 当前时间: {}", DateUtil.date());
